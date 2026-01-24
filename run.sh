4ad680da9f1c8b7a495c13d1626a173880a8a8a0
Quoc Thinh Vo <qtvo93@gmail.com>
2026-01-24 16:49:58 -0500

    update run sh

--- run.sh
+++ run.sh
@@ -6,46 +6,10 @@
 # If you refer to or use this code, in whole or in part, please consider citing the following papers:
 # @@@
 
-# show_help() {
-#     echo "Usage: ./run.sh {preprocess_features|train_model|test_model|help} [options]"
-#     echo ""
-#     echo "ACA-DTNet: Underwater Sound Source Localization"
-#     echo ""
-#     echo "Options:"
-#     echo ""
-#     echo "  preprocess_features  - Start the feature extraction"
-#     echo "                      Command: ./run.sh preprocess_features"
-#     echo ""
-#     echo "  train_model          - Train mode (test will automatically run with the option \`run_inference_mode=True\` in \`parameters.yaml\`)"
-#     echo "                      Command: ./run.sh train_model"
-#     echo ""
-#     echo "  test_model           - Test mode"
-#     echo "                      Command: ./run.sh test_model"
-#     echo ""
-# }
-
-# if [ -z "$1" ] || [ "$1" == "help" ]; then
-#     show_help
-#     exit 0
-# fi
-
-# if [ "$1" == "preprocess_features" ]; then
-#     docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml aca-dtnet-swellex96:v1 bash -c "uv run python3 underwater-ssl/process_features.py --params-file=/app/parameters.yaml"
-# elif [ "$1" == "train_model" ]; then
-#     docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml aca-dtnet-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml"
-# elif [ "$1" == "test_model" ]; then
-#     docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml aca-dtnet-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml --run-test-only"
-# else
-#     echo "Invalid option: $1"
-#     show_help
-#     exit 1
-# fi
-
-
 show_help() {
     echo "Usage: ./run.sh {preprocess_features|train_model|test_model|build_docker_image|help} [options]"
     echo ""
-    echo "ACA-DTNet: Underwater Sound Source Localization"
+    echo "SA-Net: Underwater Sound Source Localization"
     echo ""
     echo "Options:"
     echo ""
@@ -75,25 +39,25 @@ fi
 
 
 if [ "$1" == "build_docker_image" ]; then
-    docker build -t aca-dtnet-swellex96:v1 .
+    docker build -t SA-Net-swellex96:v1 .
     exit 0
 fi
 
 if [ "$1" == "preprocess_features" ]; then
     if [ "$WITH_DOCKER" == true ]; then
-        docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml aca-dtnet-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml feature_extractor"
+        docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml SA-Net-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml feature_extractor"
     else
         uv run python3 underwater-ssl/main.py --params-file=parameters.yaml feature_extractor
     fi
 elif [ "$1" == "train_model" ]; then
     if [ "$WITH_DOCKER" == true ]; then
-        docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml aca-dtnet-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml model_trainer"
+        docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml SA-Net-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml model_trainer"
     else
         uv run python3 underwater-ssl/main.py --params-file=parameters.yaml model_trainer
     fi
 elif [ "$1" == "test_model" ]; then
     if [ "$WITH_DOCKER" == true ]; then
-        docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml aca-dtnet-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml --run-test-only model_trainer"
+        docker run -it --rm -v $(pwd)/parameters.yaml:/app/parameters.yaml SA-Net-swellex96:v1 bash -c "uv run python3 underwater-ssl/main.py --params-file=/app/parameters.yaml --run-test-only model_trainer"
     else
         uv run python3 underwater-ssl/main.py --params-file=parameters.yaml --run-test-only model_trainer
     fi
